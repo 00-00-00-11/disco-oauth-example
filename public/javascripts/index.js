dbab5358e649808063e43a557f2b3d2293d62f7b
TheDrone7 <hsbhatia765@gmail.com>
2019-07-28 09:51:55 +0530

    initial completion

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,18 +1,38 @@
 var app = new Vue({
   el: "#app",
   data: {
-    userData: {}
+    userData: {},
+    guilds: [],
+    specificGuild: null
+  },
+  methods: {
+    selectGuild: function(id) {
+      console.log("Selecting Guild!");
+      this.specificGuild = this.guilds.find(i => i.id == id);
+    }
   },
   mounted() {
     fetch("/get-user")
       .then(response => response.json())
       .then(res => {
-        if (res) {
-          this.userData = res;
-        }
+        this.userData = res;
       })
-      .catch(() => {
+      .catch(e => {
+        console.log(e);
+        window.location = "/";
+      });
+    fetch("/get-guilds")
+      .then(response => response.text())
+      .then(res => {
+        this.guilds = JSON.parse(res);
+      })
+      .catch(e => {
+        console.log(e);
         window.location = "/";
       });
   }
 });
+
+function selectGuild(id) {
+  console.log("select Guild called!");
+}
--- routes/index.js
+++ routes/index.js
@@ -21,18 +21,32 @@ router.get("/auth/discord", (req, res) => {
 
 /* Route to handle incoming login requests */
 router.get("/login", async (req, res) => {
-  let key = await OAuthClient.getAccess(req.query.code);
-  res.cookie("userKey", key, {
-    maxAge: OAuthClient.getAccessObject(key).expireTimestamp - Date.now()
-  });
-
-  res.redirect("/home/");
+  try {
+    let key = await OAuthClient.getAccess(req.query.code);
+    let expires = await OAuthClient.getAccessToken(key);
+    res.cookie("userKey", key, {
+      maxAge: expires["expires_in"] * 900
+    });
+    res.redirect("/home/");
+  } catch (e) {
+    console.log(e);
+    res.end();
+  }
 });
 
 /* Route to fetch the user's details */
 router.get("/get-user", async (req, res) => {
   try {
     res.send(await OAuthClient.getAuthorizedUser(req.cookies.userKey));
+  } catch (e) {
+    res.send(e);
+  }
+});
+
+/* Route to fetch all the guilds */
+router.get("/get-guilds", async (req, res) => {
+  try {
+    res.send(await OAuthClient.getAuthorizedUserGuilds(req.cookies.userKey));
   } catch {
     res.send(undefined);
   }
